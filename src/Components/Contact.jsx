0b1580644def86a117b97c52df22c7c6163edf2c
Pablo <pablodelasierra7@gmail.com>
2025-03-26 14:33:18 +0100

    Change Contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,31 +1,36 @@
 const Contact = () => {
   return (
     <div id="Proyectos" >
-    <section id="Contacto" className="p-8 text-white">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
-        {/* Información de contacto */}
-        <div className="space-y-4 relative z-5 group ">
-          <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
-            <h1 className="text-xl font-semibold">Correo</h1>
-            <p>pablodelasierra7@gmail.com</p>
-          </div>
-          <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
-            <h1 className="text-xl font-semibold">Ubicación</h1>
-            <p>Jerez de la Frontera, Cádiz, España</p>
-          </div>
-          <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
-            <h1 className="text-xl font-semibold">Disponibilidad</h1>
-            <p>Disponible para trabajar</p>
-          </div>
-        </div>
+  <section id="Contacto" className="p-8 text-white">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl mx-auto">
+    {/* Información de contacto */}
+    <div className="space-y-4 relative z-5 group ">
+      <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
+        <h1 className="text-xl font-semibold">Correo</h1>
+        <p>pablodelasierra7@gmail.com</p>
+      </div>
+      <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
+        <h1 className="text-xl font-semibold">Ubicación</h1>
+        <p>Jerez de la Frontera, Cádiz, España</p>
+      </div>
+      <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
+        <h1 className="text-xl font-semibold">Disponibilidad</h1>
+        <p>Disponible para trabajar</p>
+      </div>
+      <div className="p-6 bg-gray-800 rounded-lg transform transition-transform duration-300 group-hover:scale-105">
+        <h1 className="text-xl font-semibold">Disponibilidad geográfica</h1>
+        <p>Disponible para mudarme dentro de España si es necesario para el puesto</p>
+      </div>
+    </div>
+
 
         {/* Formulario de contacto */}
         <div className="group">
-  <form 
-    action="https://formsubmit.co/pablodelasierra7@gmail.com" 
-    method="POST" 
-    className="bg-gray-800 p-6 rounded-lg transform transition-transform duration-300 group-hover:scale-104"
-  >
+          <form 
+            action="https://formsubmit.co/pablodelasierra7@gmail.com" 
+            method="POST" 
+            className="bg-gray-800 p-6 rounded-lg transform transition-transform duration-300 group-hover:scale-104"
+          >
           <input type="hidden" name="_next" value="https://portoflio-pablosierra.netlify.app/" />
           <input type="hidden" name="_captcha" value="false" />
 
